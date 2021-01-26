5090d5409479abb12e0e8bb8e12b6a0246342b42
prak24oct <prakharatdit@gmail.com>
2021-01-26 14:28:48 +0530

    dsmsd

---
+++ library/src/main/java/com/example/printmessage/MainPr.kt
@@ -0,0 +1,7 @@
+package com.example.printmessage
+
+class MainPr {
+
+    val new : String = "Prakhar "
+    val num : Int = 1
+}
--- library/src/main/java/com/example/printmessage/PrintActivity.kt
+++ library/src/main/java/com/example/printmessage/PrintActivity.kt
@@ -5,9 +5,19 @@ import android.os.Bundle
 
 class PrintActivity {
 
+    val name: String = "nkjdbvkjdsbv"
+    val test : Int = 1
+
+   val t =  add(5)
+
     companion object{
         fun printFunction(message : String){
         println(message)
     }}
 
+
+}
+fun add(first: Int){
+    val second : Int = 2
+    println(first+second)
 }
