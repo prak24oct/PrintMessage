cd57bdef6fa6fa3a77d02ed3879600d5328ea6c6
prak24oct <prakharatdit@gmail.com>
2021-01-26 14:10:33 +0530

    latest

---
+++ library/src/main/java/com/example/printmessage/PrintActivity.kt
@@ -0,0 +1,13 @@
+package com.example.printmessage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrintActivity {
+
+    companion object{
+        fun printFunction(message : String){
+        println(message)
+    }}
+
+}
